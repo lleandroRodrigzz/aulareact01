bfc89d94edea47204d9397016e8cb8d7f3a3a127
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2024-08-16 10:01:19 -0300

    Entendendo o ciclo de vida de um componente React

---
+++ src/meusComponentes/Data.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+export default class Data extends Component{
+
+    constructor(props){
+        super(); //Executa o construtor do pai
+        this.props = props;
+        this.dataAtual = new Date().toLocaleString();
+    }
+
+    //Fase de montagem
+    componentDidMount(){
+        console.log("O componente foi montado");
+        this.dataAtual = new Date().toLocaleString();
+    }
+
+    componentDidUpdate(){
+        console.log("O componente foi atualizado");
+    }
+
+    //Sobrescrita de método
+    render(){
+        return(
+            <h1>{this.props.texto || ""}{this.dataAtual}</h1>
+        )
+    }
+}
