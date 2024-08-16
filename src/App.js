8393b8d36c9f189b3f0e65e2dde557c9d20ef33d
lleandroRodrigzz <leandromarcoscasse@gmail.com>
2024-08-16 10:01:19 -0300

    Entendendo o ciclo de vida de um componente React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Data from "./meusComponentes/Data";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {
-            'Data/Hora em que a pagina foi carregada: ' 
-            + new Date().toLocaleString()
-          }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data texto = "Data e hora atuais: "/>
+      <Data texto = "Data e hora atuais em Londres: "/>
+      <Data texto = "Data e hora atuais em Nova York: "/>
     </div>
   );
 }
